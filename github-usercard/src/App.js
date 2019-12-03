9e12a726a7a741271fb395cec97f1d679bf0f685
D-Fink <dfink4badjuju@gmail.com>
2019-12-03 13:21:55 -0700

    mvp

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      followers: []
+    }
+  }
+  componentDidMount() {
+    Promise.all([
+      fetch("https://api.github.com/users/D-Fink"),
+      fetch("https://api.github.com/users/D-Fink/followers")
+    ])
+    .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+    .then(([data1, data2]) => this.setState({
+      user: data1,
+      followers: data2
+    }))
+  }
+  render() {
+    console.log(this.state)
+    return (
+      <div className='user'> 
+        <img src={this.state.user.avatar_url}/>
+        <h2>{this.state.user.name}</h2>
+        <p>{this.state.user.bio}</p>
+        <h3>Followers:</h3>
+        <div className='followerContainer'>
+          {this.state.followers.map(item =>
+          <div className='follower' key={item.id}>
+            <img src={item.avatar_url}/>
+            <h3>{item.login}</h3>
+            <a href={item.html_url}>Checkout profile</a>
+            <hr/>
+          </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
